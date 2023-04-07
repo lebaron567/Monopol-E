87edf1578a568776efa1e4d8461db347ea85fa21
Emeric barotin <emericbarotin@etu.univ-nantes.fr>
2023-04-07 16:23:58 +0200

    :construction: | Work in progress.

--- src/components/Grille.js
+++ src/components/Grille.js
@@ -1,12 +1,54 @@
 import React from 'react';
 
 const Grille = () => {
+
     
-    return (
-        <div>
-            
-        </div>
-    );
+    return (<div class="parent">
+    <div class="div1"  id="case" > </div>
+    <div class="div2"  id="case" > </div>
+    <div class="div3"  id="case" > </div>
+    <div class="div4"  id="case" > </div>
+    <div class="div5"  id="case" > </div>
+    <div class="div6"  id="case" > </div>
+    <div class="div7"  id="case" > </div>
+    <div class="div8"  id="case" > </div>
+    <div class="div9"  id="case" > </div>
+    <div class="div10" id="case" > </div>
+    <div class="div11" id="case" > </div>
+    <div class="div12" id="case" > </div>
+    <div class="div13" id="case" > </div>
+    <div class="div14" id="case" > </div>
+    <div class="div15" id="case" > </div>
+    <div class="div16" id="case" > </div>
+    <div class="div17" id="case" > </div>
+    <div class="div18" id="case" > </div>
+    <div class="div19" id="case" > </div>
+    <div class="div20" id="case" > </div>
+    <div class="div21" id="case" > </div>
+    <div class="div22" id="case" > </div>
+    <div class="div23" id="case" > </div>
+    <div class="div24" id="case" > </div>
+    <div class="div25" id="case" > </div>
+    <div class="div26" id="case" > </div>
+    <div class="div27" id="case" > </div>
+    <div class="div28" id="case" > </div>
+    <div class="div29" id="case" > </div>
+    <div class="div30" id="case" > </div>
+    <div class="div31" id="case" > </div>
+    <div class="div32" id="case" > </div>
+    </div>)
 };
+
+
+
+
+// class Case{
+//     constructor(x,y,tailleX,tailleY){
+//         this.x = x
+//         this.y = y
+//         this.tailleX = tailleX
+//         this.tailleY = tailleY
+//     }
+// }
 
 export default Grille;
---
+++ src/page/Game.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Grille from '../components/Grille';
+
+const Game = () => {
+    return (
+        <div>
+            <Grille />
+        </div>
+    );
+};
+
+export default Game;

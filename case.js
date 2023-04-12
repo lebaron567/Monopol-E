8dc95b4bcc4bdea7a4b04f09f651c6ba30b3ec49
Emeric barotin <emericbarotin@etu.univ-nantes.fr>
2023-04-12 14:11:59 +0200

    :construction: | set data case

--- case.js
+++ case.js
@@ -1,12 +1,22 @@
-import {stockData} from './src/data/case.js'
-
+import { stockData } from './src/data/data.js';
 class Case{
     constructor(type,axe,pos){
         this.type = type
         this.axe = axe
         this.pos = pos
     }
-}
+
+    isTerrain(){
+		return this.type === "ordinateur";
+	}
+
+	isPropriete(){
+		return this.type === "ordinateur" || this.type === "central" || this.type === "ventilation" ;
+	}
+};
+
+
+  
 
 
 console.log(stockData); 
---
+++ src/data/data.js
@@ -0,0 +1,229 @@
+export const stockData =[
+    {
+      "type": "depart",
+      "axe": 1,
+      "pos": 1
+    },{
+        "type": "ordinateur",
+        "groupe": "compaq",
+        "axe": 1,
+        "pos": 2,
+        "nom": "pc portable",
+        "prix":100,
+        "louer":20,
+        "amelioration" :[40,60,80,110],
+        "prixRAM": 30,
+        "prixCPU": 50
+    },{
+        "type": "ordinateur",
+        "groupe": "compaq",
+        "axe": 1,
+        "pos": 3,
+        "nom": "hp compad" ,      
+        "prix":130,
+        "louer":30,
+        "amelioration" :[50,70,90,150],
+        "prixRAM": 35,
+        "prixCPU": 60
+    },{
+        "type": "chance",
+        "axe": 1,
+        "pos": 4
+    },{
+        "type": "central",
+        "axe": 1,
+        "pos": 5
+    },{
+        "type": "ordinateur",
+        "groupe": "Thomson",
+        "axe": 1,
+        "pos": 6,
+        "nom": "thomson neo" ,      
+        "prix":230,
+        "louer":45,
+        "amelioration" :[90,120,160,250],
+        "prixRAM": 70,
+        "prixCPU": 150
+    },{
+        "type": "ordinateur",
+        "groupe": "Thomson",
+        "axe": 1,
+        "pos": 7,
+        "nom": "neo touch",
+        "prix":260,
+        "louer":60,
+        "amelioration" :[150,200,260,310],
+        "prixRAM": 100,
+        "prixCPU": 200
+    },{
+        "type": "ordinateur",
+        "axe": 1,
+        "pos": 8,
+        "nom": "neo 3K",
+        "prix":300,
+        "louer":100,
+        "amelioration" :[170,200,320,370],
+        "prixRAM": 130,
+        "prixCPU": 250
+    },{
+        "type": "ventre",
+        "axe": 2,
+        "pos": 1
+    },{
+        "type": "ordinateur",
+        "groupe": "dell",
+        "axe": 2,
+        "pos": 2,
+        "nom": "dell pc sff",
+        "prix":340,
+        "louer":160,
+        "amelioration" :[210,340,410,580],
+        "prixRAM": 180,
+        "prixCPU": 300
+    },{
+        "type": "ventilation",
+        "axe": 2,
+        "pos": 3
+    },{
+        "type": "ordinateur",
+        "groupe": "dell",
+        "axe": 2,
+        "pos": 4,
+        "nom": "dell pc optiplex",
+        "prix":360,
+        "louer":190,
+        "amelioration" :[230,430,630,750],
+        "prixRAM": 210,
+        "prixCPU": 330
+    },{
+        "type": "central",
+        "axe": 2,
+        "pos": 5
+    },{
+        "type": "ordinateur",
+        "groupe":"acer",
+        "axe": 2,
+        "pos": 6,
+        "nom": "Acer Aspire 3 '15,6' ",
+        "prix":410,
+        "louer":210,
+        "amelioration" :[260,500,680,810],
+        "prixRAM": 240,
+        "prixCPU": 390
+    },{
+        "type": "ordinateur",
+        "groupe":"acer",
+        "axe": 2,
+        "pos": 7,
+        "nom": "Acer Chromebook Spin",
+        "prix":500,
+        "louer":260,
+        "amelioration" :[330,590,750,920],
+        "prixRAM": 310,
+        "prixCPU": 350,
+    },{
+        "type": "ordinateur",
+        "groupe":"acer",
+        "axe": 2,
+        "pos": 8,
+        "nom": "Acer Aspire 3 '17,3'",
+        "prix":590,
+        "louer":300,
+        "amelioration" :[500,710,920,1100],
+        "prixRAM": 360,
+        "prixCPU": 380,
+    },{
+        "type": "overclocking",
+        "axe": 3,
+        "pos": 1
+    },{
+        "type": "chance",
+        "axe": 3,
+        "pos": 2
+    },{
+        "type": "ordinateur",
+        "groupe": "msi",
+        "axe": 3,
+        "pos": 3,
+        "nom": "MSI GF63 Thin 11UC",
+        "prix":750,
+        "louer":430,
+        "amelioration" :[720,950,1300,1500],
+        "prixRAM": 420,
+        "prixCPU": 450,
+
+    },{
+        "type": "ordinateur",
+        "groupe": "msi",
+        "axe": 3,
+        "pos": 4,
+        "nom": "MSI - GF66 11UC",
+        "prix":900,
+        "louer":480,
+        "amelioration" :[800,1250,1460,1680],
+        "prixRAM": 450,
+        "prixCPU": 470,
+    },{
+        "type": "central",
+        "axe": 3,
+        "pos": 5
+    },{
+        "type": "ordinateur",
+        "groupe": "asus",
+        "axe": 3,
+        "pos": 6,
+        "nom": "Asus TUF Gaming",
+        "prix":1100,
+        "louer":510,
+        "amelioration" :[890,1480,1850,2110],
+        "prixRAM": 580,
+        "prixCPU": 610,
+    },{
+        "type": "ventilation",
+        "axe": 3,
+        "pos": 7
+    },{
+        "type": "ordinateur",
+        "groupe": "asus",
+        "axe": 3,
+        "pos": 8,
+        "nom": "Asus ROG Strix G17",
+        "prix":1340,
+        "louer":580,
+        "amelioration" :[950,1560,2000,2430],
+        "prixRAM": 600,
+        "prixCPU": 670,
+    },{
+        "type": "tour du monde",
+        "axe": 4,
+        "pos": 1
+    },{
+        "type": "ordinateur",
+        "axe": 4,
+        "pos": 2
+    },{
+        "type": "ordinateur",
+        "axe": 4,
+        "pos": 3
+    },{
+        "type": "ordinateur",
+        "axe": 4,
+        "pos": 4
+    },{
+        "type": "central",
+        "axe": 4,
+        "pos": 5
+    },{
+        "type": "ordinateur",
+        "axe": 4,
+        "pos": 6
+    },{
+        "type": "taxe",
+        "axe": 4,
+        "pos": 7
+    },{
+        "type": "ordinateur",
+        "axe": 4,
+        "pos": 8
+    }
+]

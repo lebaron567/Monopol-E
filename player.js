5eb2978b52acd9dfd24c0ec8fe9c175a4d3f707e
Emeric barotin <emericbarotin@etu.univ-nantes.fr>
2023-04-14 15:02:19 +0200

    :white_check_mark:| afic player

--- player.js
+++ player.js
@@ -1,33 +1,63 @@
 export class Player{
-    constructor(){
-        this.NBcase = 0
-        this.money = 500
-        this.estate = []
-        this.axe = axe
-        this.pos = pos
-    }
+   
+    NBcase = 0
+    money = 500
+    estate = []
+    // axe = axe  a definir
+    // tpos = pos  adefinir
 
 
-    throw(){
-        const dee1 = Math.floor(Math.random() * 6);
-        const dee2 = Math.floor(Math.random() * 6);
-        console.log(`${dee1}+${dee2}=${dee1+dee2}`)
-    }
 
-    draw(){
-        c.fillStyle = 'white';
-        c.fillRect(cordener[this.NBcase].x+5,cordener[this.NBcase].y+5, 40,40);
-    }
+throw(){
+    const dee1 = Math.floor(Math.random() * 6);
+    const dee2 = Math.floor(Math.random() * 6);
+    console.log(`${dee1}+${dee2}=${dee1+dee2}`)
+}
 
-    update(){
-        if (keys.spase.pressed){
-            this.NBcase ++
-            keys.spase.pressed = false
-            c.clearRect(cordener[this.NBcase-1].x+4,cordener[this.NBcase-1].y+4, 42,42);
+draw(){
+    const cor =[6,5,4,3,2,1,0]
+    console.log("test")
+    if(axe == 1){
+        if(pos==1){
+            c.drawImage(this.image,board.width-(board.width*0.147),board.width-(board.width*0.099),50,30);
+        }else{
+            pos =cor[pos-2]
+            c.drawImage(this.image,(board.width*0.147)+(board.height*0.1*(pos-0)),board.width-(board.width*0.099),50,30)
         }
-        if(this.NBcase>35){
-            this.NBcase =0
-        }
-        this.draw();
     }
+    if(axe == 2){
+        if(pos==1){
+            c.drawImage(this.image,40,board.height-(board.height*0.12),50,30);
+        }else{
+            pos =cor[pos-2]
+            c.drawImage(this.image,30,(board.height*0.147)+(board.height*0.1*(pos-1)),50,30)
+        }
+    }
+    if(axe == 3){
+        if(pos==1){
+            c.drawImage(this.image,50,50,50,30);
+        }else{
+            c.drawImage(this.image,(board.width*0.13)+(board.height*0.1*(pos-1)),40,50,30)
+        }
+    }
+    if(axe == 4){
+        if(pos==1){
+            c.drawImage(this.image,board.width-(board.width*0.147),10,50,30);
+        }else{
+            c.drawImage(this.image,board.width-(board.width*0.12),(board.height*0.147)+(board.height*0.1*(pos-1)),50,30)
+        }
+    }
+}
+
+update(){
+    if (keys.spase.pressed){
+        this.NBcase ++
+        keys.spase.pressed = false
+        c.clearRect(cordener[this.NBcase-1].x+4,cordener[this.NBcase-1].y+4, 42,42);
+    }
+    if(this.NBcase>35){
+        this.NBcase =0
+    }
+    this.draw();
+}
 }
